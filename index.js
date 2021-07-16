dfb517e51ec22102d6195b62dd3af30542006a69
AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>
2021-07-16 14:22:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+//fs.mkdirSync('anshuman_dhiman');
+//fs.writeFileSync("anshuman_dhiman/bio.txt","I am Anshuman Dhiman");
+//fs.appendFileSync("anshuman_dhiman/bio.txt","I am in IT brach second year!!!");
+//const data = fs.readFileSync("anshuman_dhiman/bio.txt","utf8");
+//console.log(data);
+//fs.renameSync("anshuman_dhiman/bio.txt","anshuman_dhiman/newbio.txt");
+//fs.unlinkSync("anshuman_dhiman/newbio.txt");
+fs.rmdirSync("anshuman_dhiman");
